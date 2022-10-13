687e37bc6eb9dd7f0e8535b620d8e15338a3cffb
Kimberly Pham <kimberly.pham@mrjaffesclass.com>
2022-10-13 12:43:26 -0700

    10/13 Work

--- search/Sequential.java
+++ search/Sequential.java
@@ -1,6 +1,5 @@
 package search;
 
-
 /**
  * Write a description of class SequentialSearch here.
  * 
@@ -9,38 +8,36 @@ package search;
  */
 public class Sequential
 {
-  
-  /**
-   * Returns the index of the target value, or -1 if not found
-   */
-  public static int search(int[] arr, int target) {
-    // Your algorithm goes here!
-    int i = 0;
-    while (arr[i] != target && i != arr.length) {
-        i = i +1;
+
+    /**
+     * Returns the index of the target value, or -1 if not found
+     */
+    public static int search(int[] arr, int target) {
+        // Your algorithm goes here!
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] == target) {
+                return i;
+            } else {
+                i++;
+            }
+        }
+        return -1;
     }
-    if (arr[i] == target) {
-        System.out.println("Item found at " + 1);
+
+    public static void main(String[] args) {
+        int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
+                31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
+                73,54,51,25,33,20,52,79,97,70,54,63,49};
+        int left, right, mid;
+
+        printResult(82, search(arr, 82) == 25);
+        printResult(49, search(arr, 49) == arr.length-1);
+        printResult(-4, search(arr, -4) == -1);
     }
-    if (arr[i] != target || i == arr.length) {
-        System.out.println("Item not found ");
+
+    public static void printResult(int target, boolean result) {
+        System.out.println("Search for "+target+": "+((result) ? "CORRECT" : "INCORRECT"));
     }
-    return i;
-  }
-  
-  public static void main(String[] args) {
-    int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
-      31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
-      73,54,51,25,33,20,52,79,97,70,54,63,49};
-    int left, right, mid;
-    
-    printResult(82, search(arr, 82) == 25);
-    printResult(49, search(arr, 49) == arr.length-1);
-    printResult(-4, search(arr, -4) == -1);
-  }
-  
-  public static void printResult(int target, boolean result) {
-    System.out.println("Search for "+target+": "+((result) ? "CORRECT" : "INCORRECT"));
-  }
 
 }
